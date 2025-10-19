7d3780cb1ec9f4476068f90c30637cb899885f5f
Shakeeb Shaikh <eren.85yeager@gmail.com>
2025-10-19 10:38:21 +0530

    Implement twoSum function using HashMap
    The Tow Sum problem is solved Using  Hashmap

---
+++ 1. Two Sum/TwoSum.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        hashMap ={}
+        for i,num in enumerate(nums):
+            diff=target-num
+            if diff in hashMap:
+                return [hashMap[diff],i]
+            hashMap[num]=i
+            
+        
